054ffd85831e1343f0ec0b6d95c6839b4f1c53bf
mytech7812 <mytechjourney@aol.com>
2026-06-22 17:21:04 +0100

    more cors fix

--- api/upload.js
+++ api/upload.js
@@ -7,14 +7,12 @@ export default async function handler(req, res) {
 
   try {
     // Generate a signed URL using put with a placeholder
-    // The client will replace this placeholder with the actual file
     const { url } = await put('app.zip', 'placeholder', {
       access: 'public',
       contentType: 'application/zip',
       addRandomSuffix: false,
     });
 
-    // Return the URL – the client library expects a "url" property
     res.status(200).json({ url });
   } catch (error) {
     console.error('Signed URL error:', error);
